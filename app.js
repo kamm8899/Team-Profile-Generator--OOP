6d68d002f79f2bc06c74d223cac559864ba108ea
kamm8899 <kamm8899@gmail.com>
2021-08-22 05:59:16 -0700

    adding code for engineer and Manager

--- app.js
+++ app.js
@@ -9,29 +9,45 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./Develop/lib/htmlRenderer");
-let employeeArray =[];
+const { exit } = require("yargs");
+let employeeArray = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-const engineerQuestions =
-    inquirer
-        .prompt([{
-                type: "input",
-                name: "github",
-                message: "What is your github username?",
-                validate: github => {
-                    if (github) {
-                      return true;
-                    } else {
-                      console.log('Please add a valid github username');
-                      return false;
-                }
-            } 
-                },
+function engineerQuestions(employeeAnswers) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "github",
+        message: "What is your github username?",
+        validate: (github) => {
+          if (github) {
+            return true;
+          } else {
+            console.log("Please add a valid github username");
+            return false;
+          }
+        },
+      },
+    ])
+    .then((engineerAnswers) => {
+      console.log(employeeAnswers);
+      console.log(engineerAnswers);
+      const engineer = new Engineer(
+        employeeAnswers.name,
+        employeeAnswers.id,
+        employeeAnswers.emailAddress,
+        engineerAnswers.github
+      );
+      employeeArray.push(engineer);
+      console.log(employeeArray);
+      //go back to the intial prompt
+      roleQuestion();
+    });
+}
 
-        ]);
-
-    const managerQuestions =
+    function managerQuestions(employeeAnswers) {
         inquirer
             .prompt([{
                     type: "input",
@@ -44,11 +60,26 @@ const engineerQuestions =
                           console.log('Please enter valid Phone number?');
                           return false;
                     }
-                } 
+                }
                     },
-            ]);
-    
-    const internQuesiton =
+            ])
+            .then((managerAnswers) => {
+                console.log(employeeAnswers);
+                console.log(managerAnswers);
+                const manager = new Manager(
+                  employeeAnswers.name,
+                  employeeAnswers.id,
+                  employeeAnswers.emailAddress,
+                  managerAnswers.officeNumber
+                );
+                employeeArray.push(manager);
+                console.log(employeeArray);
+                //go back to the intial prompt
+                roleQuestion();
+              });
+        };
+
+    function internQuesiton(){
         inquirer
             .prompt([{
                     type: "input",
@@ -61,93 +92,98 @@ const engineerQuestions =
                           console.log('Please enter valid school name?');
                           return false;
                     }
-                } 
-                    }, 
-            ]);
+                }
+                    },
+            ])
+        };
 
-
-const getEmployee = 
-inquirer 
-    .prompt([{
+function getEmployee(roleQuestion) {
+  inquirer
+    .prompt([
+      {
         type: "input",
         name: "name",
-        message: "What is employee's name?"
-    },
-    {
-    type: "input",
-    name: "id",
-    message: "What is the employee id?",
-    validate: id => {
-        if (id) {
-          return true;
-        } else {
-          console.log('Please select a valid id');
-          return false;
-    }
-} 
-    },
-    {
+        message: "What is employee's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the employee id?",
+        validate: (id) => {
+          if (id) {
+            return true;
+          } else {
+            console.log("Please select a valid id");
+            return false;
+          }
+        },
+      },
+      {
         type: "input",
         name: "emailAddress",
         message: "What is employee's email?",
-        validate: emailAddress => {
-            if (emailAddress) {
-              return true;
-            } else {
-              console.log('Enter your email!!');
-              return false;
-        }
-    }
-},
-{
-    type: "list",
-    name: "role",
-    message: "What is employee's role?",
-    choices:[
-        "Engineer",
-        "Intern",
-        "Manager",
-    ],
-    },
-])
-.then(employeeAnswers =>{
-    if(employeeAnswers.role === "Engineer"){
-        engineerQuestions();
-    }else if(employeeAnswers.role === "Manager"){
-        managerQuestions();
-    }else if(employeeAnswers.role === "Intern"){
-        internQuestions();
-    }
-})
+        validate: (emailAddress) => {
+          if (emailAddress) {
+            return true;
+          } else {
+            console.log("Enter your email!!");
+            return false;
+          }
+        },
+      },
+    ])
 
-
-.then(employeeAnswers =>{
-    console.log(employeeAnswers);
-    if(employeeAnswers.role === "Engineer"){
-        const engineer = new Engineer(employeeAnswer.name, employeeAnswer.id, employeeAnswer.github)
-        employeeArray.push(engineer);
-    }
-    // else if(employeeAnswers.role === "Manager"){
-    //     const manager = new Engineer(employeeAnswer.name, employeeAnswer.id, employeeAnswer.github)
-    //     employeeArray.push(employeeAnswers);
-    // }else if(employeeAnswers.role === "Intern"){
-    //     employeeArray.push(employeeAnswers);
-    //}
-    member(employeeArray);
-});
-
-
-function member(){
-    fs.existsSync(OUTPUT_DIR) || fs.mkdirSync(OUTPUT_DIR);
-    fs.writeFileSync(outputPath, render(getEmployee), "UTF-8");
+    .then((employeeAnswers) => {
+      console.log(employeeAnswers);
+      if (roleQuestion === "Engineer") {
+        console.log("running engineer prompt");
+        engineerQuestions(employeeAnswers);
+      } else if (roleQuestion === "Manager") {
+        console.log("running manager");
+            managerQuestions(employeeAnswers);
+      } else if (roleQuestion === "Intern") {
+        console.log("running intern");
+         internQuestions(employeeAnswers);
+      }
+    });
+}
+function roleQuestion() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "What is employee's role?",
+        choices: ["Engineer", "Intern", "Manager", "EXIT"],
+      },
+    ])
+    .then((roleQuestion) => {
+      console.log(roleQuestion);
+      if (roleQuestion.role === "EXIT") {
+        member();
+      } else {
+        getEmployee(roleQuestion.role);
+      }
+    });
 }
 
+roleQuestion();
 
 
+//     // else if(employeeAnswers.role === "Manager"){
+//     //   const manager = new Manager(employeeAnswer.name, employeeAnswer.id, employeeAnswer.officeNumber, employeeAnswer.emailAddress);
+//     //      employeeArray.push(manager);
+//     //  }else if(employeeAnswers.role === "Intern"){
+//     //      const intern = new Intern(employeeAnswer.name, employeeAnswer.id, employeeAnswer.school,employeeAnswer.emailAddress);
+//     //      employeeArray.push(employeeAnswers);
+//     // }
+//     // member(employeeArray);
+// });
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+function member() {
+  fs.existsSync(OUTPUT_DIR) || fs.mkdirSync(OUTPUT_DIR);
+  fs.writeFileSync(outputPath, render(getEmployee), "UTF-8");
+}
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
@@ -155,16 +191,11 @@ function member(){
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
-
 
 // GIVEN a command-line application that accepts user input
 // WHEN I am prompted for my team members and their information
